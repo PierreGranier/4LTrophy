56e0b265f4474b0864e2b8749e9485b5477ae341
unknown <thibaut.roperch@free.fr>
2018-09-09 15:21:06 +0200

    Tri des photos de la galerie par date décroissante

--- php/galerie.php
+++ php/galerie.php
@@ -5,18 +5,25 @@ header('Content-Type: application/json');
 $directory_name = "galerie/";
 
 $directory_position = "../" . $directory_name;
-$images["records"] = [];
+$files_sorted_by_date = [];
 
 if (is_dir($directory_position)) {
     $directory = opendir($directory_position);
 
     while (($file = readdir($directory)) !== false) {
         if (is_file($directory_position . $file)) {
-            array_push($images["records"], $directory_name . $file);
+            $files_sorted_by_date[filemtime($directory_position . $file)] = $file;
         }
     }
 
     closedir($directory);
+}
+
+krsort($files_sorted_by_date);
+
+$images["records"] = [];
+foreach ($files_sorted_by_date as $file) {
+    array_push($images["records"], $directory_name . $file);
 }
 
 echo json_encode($images);
